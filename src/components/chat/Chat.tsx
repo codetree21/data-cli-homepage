ac88119dfdac062cc93f9af42b53c4ec5266b943
chavokim <treeboy0384@gmail.com>
2023-05-18 10:04:27 +0900

    style: loading

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { VimInput } from "../../components/viminput";
 import { useQuery } from "react-query";
 import axios from "axios";
+import { useTheme } from "src/utils/themeProvider";
 
 const Chat = ({ inputRef }) => {
 	const { data: chats } = useQuery({
@@ -13,9 +14,36 @@ const Chat = ({ inputRef }) => {
 		refetchInterval: 1000,
 	});
 
+	const theme = useTheme();
+
 	return (
 		<div className={"relative h-full"}>
-			<p className={"whitespace-pre-wrap"}>{chats?.join("\n")}</p>
+			{chats ? (
+				<p className={"whitespace-pre-wrap"}>{chats?.join("\n")}</p>
+			) : (
+				<div className="animate-pulse flex flex-col space-y-2">
+					<div className="grid grid-cols-12 gap-2">
+						<div className="h-4 bg-current rounded col-span-1"></div>
+						<div className="h-4 bg-current rounded col-span-3"></div>
+					</div>
+					<div className="grid grid-cols-12 gap-2">
+						<div className="h-4 bg-current rounded col-span-1"></div>
+						<div className="h-4 bg-current rounded col-span-3"></div>
+					</div>
+					<div className="grid grid-cols-12 gap-2">
+						<div className="h-4 bg-current rounded col-span-1"></div>
+						<div className="h-4 bg-current rounded col-span-3"></div>
+					</div>
+					<div className="grid grid-cols-12 gap-2">
+						<div className="h-4 bg-current rounded col-span-1"></div>
+						<div className="h-4 bg-current rounded col-span-3"></div>
+					</div>
+					<div className="grid grid-cols-12 gap-2">
+						<div className="h-4 bg-current rounded col-span-1"></div>
+						<div className="h-4 bg-current rounded col-span-3"></div>
+					</div>
+				</div>
+			)}
 			<div className={"fixed bottom-3 w-[calc(100%-48px)]"}>
 				<VimInput inputRef={inputRef} />
 			</div>
